46b3f99141806ad6830a7179c227201ed02b3937
alissondevilla <alisson_dg4@hotmail.com>
2021-07-18 18:55:39 -0300

    Organização código

--- exercicio1/src/exercicio1/Exercicio1.java
+++ exercicio1/src/exercicio1/Exercicio1.java
@@ -1,22 +1,21 @@
 package exercicio1;
 
 public class Exercicio1 {
-
 	static int totalEleitores = 1000;
 	static float percentual = 0;
-
-    private static float percentualVotosValidos(){
-    	int votosValidos = 800;
-
-    	percentual = (float) votosValidos / totalEleitores;
-    	percentual *= 100;
-    	
-    	return percentual;
+	
+	private static float percentualVotosValidos(){
+		int votosValidos = 800;
+		
+		percentual = (float) votosValidos / totalEleitores;
+		percentual *= 100;
+		
+		return percentual;
 	}
 
 	private static float percentualVotosBrancos(){
 		int votosBrancos = 150;
-
+		
 		percentual = (float) votosBrancos / totalEleitores;
 		percentual *= 100;
 		
@@ -25,21 +24,21 @@ public class Exercicio1 {
 	
 	private static float percentualVotosNulos(){
 		int votosNulos = 50;
-
+		
 		percentual = (float) votosNulos / totalEleitores;
 		percentual *= 100;
-
+		
 		return percentual;
 	}
-
-    public static void main(String[] args) {
-    	float percentualVotosValidos = 0;
-    	float percentualVotosBrancos = 0;
-    	float percentualVotosNulos = 0;
-
-        percentualVotosValidos = percentualVotosValidos();
-        percentualVotosBrancos = percentualVotosBrancos();
-        percentualVotosNulos = percentualVotosNulos();
+	
+	public static void main(String[] args) {
+		float percentualVotosValidos = 0;
+		float percentualVotosBrancos = 0;
+		float percentualVotosNulos = 0;
+		
+		percentualVotosValidos = percentualVotosValidos();
+		percentualVotosBrancos = percentualVotosBrancos();
+		percentualVotosNulos = percentualVotosNulos();
 
     	System.out.println("Percentual de votos válidos em relação ao total de eleitores: " + percentualVotosValidos + "%");
     	System.out.println("Percentual de votos em branco em relação ao total de eleitores: " + percentualVotosBrancos + "%");
